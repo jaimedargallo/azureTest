350c2871f075820fdacbbb794f7d9391831a94ae
Jaime Dargallo <jaimedargallo@gmail.com>
2023-10-03 14:51:32 +0200

    jekyll config

---
+++ _site/public/script.js
@@ -0,0 +1,48 @@
+// Cargar el archivo JSON
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "public/preguntas.json");
+xhr.onload = function() {
+  // Parsear el archivo JSON
+  const preguntas = JSON.parse(xhr.responseText);
+
+  // Agregar una variable para almacenar la respuesta del usuario
+  let respuestaUsuario;
+
+  // Mostrar la pregunta
+  const divPregunta = document.createElement("div");
+  divPregunta.innerHTML = `
+    <h2>${preguntas[0].pregunta}</h2>
+    <ul>
+      <li>${preguntas[0].respuestas[0]}</li>
+      <li>${preguntas[0].respuestas[1]}</li>
+      <li>${preguntas[0].respuestas[2]}</li>
+      <li>${preguntas[0].respuestas[3]}</li>
+    </ul>
+  `;
+  document.body.main.appendChild(divPregunta);
+
+  // Obtener la respuesta del usuario
+  const btnRespuesta = document.querySelector("input[type='radio']:checked");
+  respuestaUsuario = btnRespuesta.value;
+
+  // Comprobar la respuesta del usuario
+  const isCorrecta = comprobarRespuesta(respuestaUsuario, preguntas[0].respuestaCorrecta);
+
+  // Mostrar un mensaje de respuesta
+  mostrarMensajeRespuesta(isCorrecta);
+};
+xhr.send();
+
+// Función para comprobar la respuesta del usuario
+function comprobarRespuesta(respuestaUsuario, respuestaCorrecta) {
+  return respuestaUsuario === respuestaCorrecta;
+}
+
+// Función para mostrar un mensaje de respuesta
+function mostrarMensajeRespuesta(isCorrecta) {
+  if (isCorrecta) {
+    alert("Respuesta correcta");
+  } else {
+    alert("Respuesta incorrecta");
+  }
+}
